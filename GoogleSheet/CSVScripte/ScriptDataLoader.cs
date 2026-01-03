b4e7e9a3228f8a3c2c4ddb24ff0834f61583add3
tjdqls1258 <42854743+tjdqls1258@users.noreply.github.com>
2026-01-03 21:06:31 +0900

    Update ScriptDataLoader.cs
    로컬, 어드레서블로드 추가

--- GoogleSheet/CSVScripte/ScriptDataLoader.cs
+++ GoogleSheet/CSVScripte/ScriptDataLoader.cs
@@ -1,3 +1,4 @@
+using Cysharp.Threading.Tasks;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -6,13 +7,15 @@ using UnityEngine;
 
 public static class ScriptDataLoader<DATA> where DATA : CSVData, new()
 {
-    readonly static string CSV_PATH = Application.dataPath + "/GoogleSheet/CSVData/";
+    readonly static string CSV_PATH = "Assets/Util/GoogleSheet/CSVData/";
 
     public static Dictionary<int, DATA> ReadFile((string, Type) strFileType, ICsvListHelper csvList) 
     {
-        StreamReader reader = new StreamReader(CSV_PATH + strFileType.Item1 + ".csv");
-        
+        string reader = strFileType.Item1;
+        string[] dataList = reader.Split("\n");
+        int currentLine = 0;
         bool isLast = false;
+
         Type type = strFileType.Item2;
         FieldInfo[] fieldInfos = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
         Dictionary<int, DATA> result = new();
@@ -20,12 +23,15 @@ public static class ScriptDataLoader<DATA> where DATA : CSVData, new()
         while (!isLast)
         {
             DATA data = new();
-            string datas = reader.ReadLine();
-            if (datas == null)
+            
+            if (currentLine >= dataList.Length)
             {
                 isLast = true;
                 break;
             }
+
+            string datas = dataList[currentLine];
+            currentLine++;
 
             var dataSplit = datas.Split(',');
 
@@ -72,23 +78,44 @@ public static class ScriptDataLoader<DATA> where DATA : CSVData, new()
 
 public class CSVHelper
 {
+    private const string PATH = "CSVData/{0}.csv";
+    private readonly string PATH_LOCAL = $"{Application.dataPath}/Util/GoogleSheet/CSVData/{{0}}.csv";
     protected Dictionary<Type, object> m_scriptDataList = new();
 
     public enum CSVFile
     {
-        
+        CharacterData
     }
 
     private readonly (CSVFile, ICsvListHelper)[] m_csvData =
     {
-
+        (CSVFile.CharacterData ,(new CharacterDataList())),
     };
 
     public void InitCSVData()
     {
         foreach(var data in m_csvData)
         {
-            data.Item2.SetDatas(data.Item1.ToString());
+            StreamReader reader = new StreamReader(string.Format(PATH_LOCAL, data.Item1.ToString()));
+            data.Item2.SetDatas(reader.ReadToEnd());
+            m_scriptDataList.Add(data.Item2.GetType(), data.Item2);
+        }
+    }
+
+    public async UniTask InitCSVDataAsync()
+    {
+        List<UniTask> TaskList = new(); 
+        foreach(var data in m_csvData)
+        {
+            TaskList.Add(InitItem(data));
+        }
+
+        await UniTask.WhenAll(TaskList);
+
+        async UniTask InitItem((CSVFile, ICsvListHelper) data)
+        {
+            var file = await AddressableManager.Instance.LoadAssetAndCacheAsync<TextAsset>(string.Format(PATH, data.Item1.ToString()));
+            data.Item2.SetDatas(file.text);
             m_scriptDataList.Add(data.Item2.GetType(), data.Item2);
         }
     }
@@ -108,9 +135,14 @@ public class CSVDataList<Data> : ICsvListHelper where Data : CSVData, new()
 {
     protected Dictionary<int, Data> m_dataList = new();
 
-    public virtual void SetDatas(string fileName)
+    public virtual void SetDatas(TextAsset file)
     {
-        m_dataList = ScriptDataLoader<Data>.ReadFile((fileName, typeof(Data)), this);
+        m_dataList = ScriptDataLoader<Data>.ReadFile((file, typeof(Data)), this);
+    }
+
+    public virtual void SetDatas(TextAsset file)
+    {
+        m_dataList = ScriptDataLoader<Data>.ReadFile((file, typeof(Data)), this);
     }
 
     public virtual Data GetData(int id)
@@ -121,5 +153,6 @@ public class CSVDataList<Data> : ICsvListHelper where Data : CSVData, new()
 
 public interface ICsvListHelper
 {
-    public void SetDatas(string fileName);
+    public void SetDatas(TextAsset csvFile);
+    public void SetDatas(string csvFilePath);
 }
